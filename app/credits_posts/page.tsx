dd9283676b576b76ccd0c5814bdbe71b69361a07
umeshSinghVerma <usv0308@gmail.com>
2023-08-23 18:56:51 +0530

    credit_posts page complete

--- app/credits_posts/page.tsx
+++ app/credits_posts/page.tsx
@@ -3,76 +3,127 @@ import React from 'react'
 
 const photoData = [
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/213897953_10226234773878251_2231240478267296904_n_wed_thumb.jpg",
+      coupleName: "ניצן וגלעד",
+      date: "חתונה בגן פתוח"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/80889052_10221749269621091_7627648075569299456_o_wed_thumb.jpg",
+      coupleName: "ניצן ורועי",
+      date: "מקום התארגנות"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/74593549_10157830621794772_3455503559901052928_o_wed_thumb.jpg",
+      coupleName: "קארין ואופיר",
+      date: "מקום התארגנות"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/271869942_10228537670205990_4481067787529923997_n_wed_thumb.jpg",
+      coupleName: "גילי ואיתי",
+      date: "חתונה בגן פתוח"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/184191737_10223380841003558_8365871247580097655_n_wed_thumb.jpg",
+      coupleName: "שירה ורון",
+      date: "חתונת צהריים"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/340760348_231517582859049_3552259639863266206_n_wed_thumb.jpg",
+      coupleName: "עדי ושחר",
+      date: "חתונה בגן פתוח"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/355457353_6239087042852520_4465639580556907374_n_wed_thumb.jpg",
+      coupleName: "שירה ולאוניד",
+      date: "פוסט מושקע במיוחד"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/240996950_10228006710499957_3525380486826726790_n_wed_thumb.jpg",
+      coupleName: "יהודית וליאור",
+      date: "צלמי הפינצטה"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/197752320_10222833633284162_2915410483728005119_n_wed_thumb.jpg",
+      coupleName: "שיר ואלון",
+      date: "חתונה מול הים"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/239536117_10223722600245598_2370804054457578837_n_wed_thumb.jpg",
+      coupleName: "טל וטל",
+      date: "חתונת DIY"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/248502513_10223368092262031_1401770360066167163_n_wed_thumb.jpg",
+      coupleName: "שני ואידור",
+      date: "צלמי הפינצטה"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/80805200_2882748625088669_1783501237136654336_n_(1)_wed_thumb.jpg",
+      coupleName: "ספיר וגיא",
+      date: "כלה מתולתלת"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/299729817_8565528113461013_7591435139407118517_n_wed_thumb.jpg",
+      coupleName: "ענבר ושגיב",
+      date: "חתונה בגן פתוח"
     },
     {
-        imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/81500110_10213009893317106_5526315848324612096_n_wed_thumb.jpg",
-        coupleName: "ספיר ולידור",
-        date: "מקום התארגנות"
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/2019/70923869_10157518914997180_8956386561813905408_o2_wed_thumb.jpg",
+      coupleName: "דניאל גולד",
+      date: ""
     },
-]
+    {
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/292720621_10228994053940848_5744001089685407301_n_wed_thumb.jpg",
+      coupleName: "יהל וטל",
+      date: "חתונת צהריים"
+    },
+    {
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/184738765_10159194054104288_2196347665229366578_n_wed_thumb.jpg",
+      coupleName: "נועה ויוסי",
+      date: "פוסט מושקע במיוחד"
+    },
+    {
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/131377857_10221933836067720_1488534264417217479_n_wed_thumb.jpg",
+      coupleName: "לי-אור ומנור",
+      date: "חתונה בגן פתוח"
+    },
+    {
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/345431463_261770306227502_5555827717960910192_n_wed_thumb.jpg",
+      coupleName: "שובל ואיתן",
+      date: "חתונה בגן פתוח"
+    },
+    {
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/200346820_3916119275124040_742805586906725759_n_wed_thumb.jpg",
+      coupleName: "גל ועמית",
+      date: "פוסט מושקע במיוחד"
+    },
+    {
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/105032388_10214706568731767_2154468490014875619_o_wed_thumb.jpg",
+      coupleName: "יאנה ומקסים",
+      date: "חתונה קטנה"
+    },
+    {
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/349772447_644720530457347_9111117364532968706_n_wed_thumb.jpg",
+      coupleName: "יובל ואהרון",
+      date: "חתונה בגן פתוח"
+    },
+    {
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/205185690_10224985790142274_6368941156407049467_n_wed_thumb.jpg",
+      coupleName: "ירדן וגיא",
+      date: "חתונה בשפלה"
+    },
+    {
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/204087964_4450405328303677_43650119581559470_n_wed_thumb.jpg",
+      coupleName: "יאנה ודניאל",
+      date: "חתונה בדרום"
+    },
+    {
+      imageUrl: "https://www.wedreviews.co.il/uploads/default/files/credits_posts/82191980_10157713668839178_6041607946981343232_o_wed_thumb.jpg",
+      coupleName: "נופר ושלומי",
+      date: "חתונה בשרון"
+    }
+  ];
+   
 
 export default function page() {
     return (
@@ -136,7 +187,7 @@ export default function page() {
                     <img src="/searchicon.svg" alt="" className='h-[80%] mt-2' />
                 </div>
             </div>
-            <div className="tags flex flex-wrap">
+            <div className="tags flex flex-wrap mt-[17px]">
                 <div
                     className="tag"
                     data-acsb-clickable="true"
